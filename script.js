6d996f0f34df4461ac747780e2d7b3b24413f4a6
Yuri Araújo <71369804+sempiternus-bit@users.noreply.github.com>
2022-07-06 01:33:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const mario = document.querySelector('.mario')
+const tubo = document.querySelector('.tubo')
+
+const jump = () => {
+
+	mario.classList.add('jump')
+
+	setTimeout(() => {
+
+		mario.classList.remove('jump')		
+
+	}, 500)
+
+}
+
+const loop = setInterval(() => {
+
+	const tubopos = tubo.offsetLeft
+	const mariopos = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+	if(tubopos <= 56 && tubopos > 0 && mariopos < 80){
+
+		tubo.style.animation = 'none'
+		tubo.style.left = `${tubopos}px`
+
+		mario.style.animation = 'none'
+		mario.style.bottom = `${mariopos}px`
+
+		mario.src = 'gameover.png'
+		mario.style.width = '6%'
+
+		clearInterval(loop)
+
+	} 
+
+}, 10)
+
+
+document.addEventListener('keydown', jump)
